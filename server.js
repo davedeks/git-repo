cb7fa508750c696f9f3bbda5a2d21f3caacbc660
--David Deko <--davedeksy@yahoo.com>
2023-08-30 17:01:09 -0500

    udt4

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const hostname = 'http://nodeproject3-nodeproject3.192.168.99.100.nip.io/';
-const port = 3000;
+const port = 80;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
